00f9d94551c2b233353e8a6fd8a23b0b85cd717d
Rahmath-Unnisa <asfiarehmath@gmail.com>
2022-04-14 15:03:38 +0530

    [Rahmath]Add: Computing Fibbonacci Series

---
+++ LogicalPrograms/FibbonacciSeries.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class FibbonacciSeries
+    {
+        int f1 = 0, f2 = 1, f3 = 0;
+        public int series()
+        {
+            Console.WriteLine("Enter the number:");
+            int num=Convert.ToInt32(Console.ReadLine());
+            for(int i=1;i<=num;i++)
+            {
+                f3 = f1 + f2;
+                f1 = f2;
+                f2 = f3;
+                Console.WriteLine(f1);
+
+            }
+            return f1;
+        }
+        
+    }
+}
---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace LogicalPrograms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------------Welcome To Logical Programs------------------------");
+            bool end = true;
+            Console.WriteLine("n1. Fibbonacci Series \n2. End of the program");
+            while (end==true)
+            {
+                Console.WriteLine("Enter the option to execute the program ");
+                int option= Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        FibbonacciSeries fibo = new FibbonacciSeries();
+                        fibo.series();
+                        break;
+                    case 2:
+                        end = false;
+                        break;
+                    default: Console.WriteLine("Enter the proper option to execute");
+                        break;
+                }
+            }
+        }
+    }
+}
+
+
+    
